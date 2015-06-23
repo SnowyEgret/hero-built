afed5cd5961bea0a5e6a38f87a17ae15f5af3b1d
SnowyEgret <duncan.m.swain@gmail.com>
2015-06-23 10:21:10 -0400

    Replace classes BlockPicked/SelectedProperty with PrevStateProperty #174

---
+++ src/main/java/org/snowyegret/mojo/block/PrevStateProperty.java
@@ -0,0 +1,28 @@
+package org.snowyegret.mojo.block;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraftforge.common.property.IUnlistedProperty;
+
+public class PrevStateProperty implements IUnlistedProperty<IBlockState> {
+
+	@Override
+	public String getName() {
+		return "PrevStateProperty";
+	}
+
+	@Override
+	public boolean isValid(IBlockState value) {
+		return true;
+	}
+
+	@Override
+	public Class<IBlockState> getType() {
+		return IBlockState.class;
+	}
+
+	@Override
+	public String valueToString(IBlockState value) {
+		return value.toString();
+	}
+
+}
