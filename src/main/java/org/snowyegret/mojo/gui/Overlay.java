b3e088a442dc36c58f77bdf4be83e64edb89a6c8
SnowyEgret <duncan.m.swain@gmail.com>
2015-07-14 16:38:55 -0400

    Spell info description blank if zero string in lang file #250

--- src/main/java/org/snowyegret/mojo/gui/Overlay.java
+++ src/main/java/org/snowyegret/mojo/gui/Overlay.java
@@ -38,23 +38,26 @@ public class Overlay {
 	}
 
 	public void setMessage(String message) {
-		this.message  = message;
+		this.message = message;
 	}
 
 	// Called by ForgeEventHandler.onRenderGameOverlayEvent on the client side
 	public void drawSpell(Spell spell, Player player) {
-	
+
 		int x = 10;
 		int y = x;
 		FontRenderer r = Minecraft.getMinecraft().fontRendererObj;
 		int rh = r.FONT_HEIGHT + 5;
-	
+
 		SpellInfo info = spell.getInfo();
 		r.drawStringWithShadow(info.getName().toUpperCase(), x, y, WHITE);
-		r.drawStringWithShadow(info.getDescription(), x, y += rh, WHITE);
+		String description = info.getDescription();
+		if (!description.isEmpty()) {
+			r.drawStringWithShadow(description, x, y += rh, WHITE);
+		}
 		r.drawStringWithShadow(info.getPicks(), x, y += rh, GREEN);
 		r.drawStringWithShadow(info.getModifiers(), x, y += rh, BLUE);
-	
+
 		// Display the dimensions of the impending volume if player is picking or shift selecting
 		boolean isPicking = EventHandlerClient.pickInfo.isPicking();
 		if (isPicking || (!isPicking && Modifier.SHIFT.isPressed())) {
@@ -73,25 +76,25 @@ public class Overlay {
 				}
 			}
 		}
-	
+
 		if (EventHandlerClient.selectionInfo.getSize() != 0) {
 			r.drawStringWithShadow("Selections: " + EventHandlerClient.selectionInfo.getSize(), x, y += rh, RED);
 		}
-	
+
 		// TODO SpellFillRandom should set message
 		if (spell instanceof SpellFillRandom) {
 			r.drawStringWithShadow(player.getHotbar().toString(), x, y += rh, BLUE);
 		}
-	
+
 		if (MouseHandler.isOrbiting) {
 			r.drawStringWithShadow(player.getDirection().toString().toLowerCase(), x, y += rh, BLUE);
 		}
-	
+
 		if (distance != null) {
 			String s = String.format("Distance: %.1f", distance);
 			r.drawStringWithShadow(s, x, y += rh, GREEN);
 		}
-		
+
 		if (message != null) {
 			r.drawStringWithShadow(message, x, y += rh, RED);
 		}
